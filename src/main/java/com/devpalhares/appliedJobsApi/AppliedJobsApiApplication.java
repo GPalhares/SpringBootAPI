86f73a86e84d10a7a90cbb5e47e479e96e0c6e9e
Gabriel <devpalhares@gmail.com>
2023-06-26 19:08:37 -0300

    Starting Project

---
+++ src/main/java/com/devpalhares/appliedJobsApi/AppliedJobsApiApplication.java
@@ -0,0 +1,13 @@
+package com.devpalhares.appliedJobsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliedJobsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliedJobsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpalhares/appliedJobsApi/AppliedJobsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpalhares.appliedJobsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliedJobsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
