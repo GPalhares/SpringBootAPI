c5dc3c796f2b87d48556ddc28bed9a8d988e12fc
Gabriel <devpalhares@gmail.com>
2023-06-26 22:06:13 -0300

    feat: creating database with java spring

--- src/main/java/com/devpalhares/appliedJobsApi/AppliedJobsApiApplication.java
+++ src/main/java/com/devpalhares/appliedJobsApi/AppliedJobsApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppliedJobsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppliedJobsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppliedJobsApiApplication.class, args);
+    }
 
 }
