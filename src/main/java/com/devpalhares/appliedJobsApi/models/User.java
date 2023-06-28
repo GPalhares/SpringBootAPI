416b84089f73ca70d01bfa89de5bba88e715779f
Gabriel <devpalhares@gmail.com>
2023-06-27 21:35:04 -0300

    feat: Class User

--- src/main/java/com/devpalhares/appliedJobsApi/models/User.java
+++ src/main/java/com/devpalhares/appliedJobsApi/models/User.java
@@ -1,21 +1,94 @@
 package com.devpalhares.appliedJobsApi.models;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 @Table(name = "user")
 public class User {
+    public interface CreateUser {
+    }
 
+    public interface UpdateUser {
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 4, max = 50)
     @Column(name = "username", length = 50, nullable = false)
     private String username;
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 4, max = 100)
     @Column(name = "email", length = 100, nullable = false, unique = true)
     private String email;
-    @Column(name = "password", length = 80, nullable = false)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 30)
+    @Column(name = "password", length = 30, nullable = false)
     private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        User user = (User) object;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, password);
+    }
+    //    private List<Task> tasks = new ArrayList<Task>();
 }
