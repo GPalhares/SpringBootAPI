e055eabc31ae07725237a41e5bf7c0fed61b24e0
Gabriel <devpalhares@gmail.com>
2023-06-30 20:31:21 -0300

    feat: Create User Service

---
+++ src/main/java/com/devpalhares/appliedJobsApi/services/UserService.java
@@ -0,0 +1,51 @@
+package com.devpalhares.appliedJobsApi.services;
+
+import com.devpalhares.appliedJobsApi.models.User;
+import com.devpalhares.appliedJobsApi.respositories.ApplicationsRepository;
+import com.devpalhares.appliedJobsApi.respositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.management.RuntimeErrorException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ApplicationsRepository applicationsRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "User not found ID:" + id + "Data Type" + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.applicationsRepository.saveAll(obj.getApplications());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Not possible to delete User");
+        }
+    }
+}
